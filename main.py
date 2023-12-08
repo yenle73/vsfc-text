86ea98a35794781bb9da064bdbe3913f3192739d
yenle73 <128300242+yenle73@users.noreply.github.com>
2023-12-08 08:13:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import streamlit as st
+import pickle as pkl
+import numpy as np
 
+class_list = {'0': 'Negative', '1': 'Positive', '2': 'Neutral'}
+
+st.title('Sentiment Analysis')
+
+text = st.text_input('Enter text')
+
+if text is not None:
+  
