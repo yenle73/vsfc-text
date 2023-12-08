b1e5429054cf8c65329c0771396e943642182e4f
yenle73 <128300242+yenle73@users.noreply.github.com>
2023-12-08 08:16:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pickle as pkl
 import numpy as np
 
 class_list = {'0': 'Negative', '1': 'Positive', '2': 'Neutral'}
+with open('ec_vsfc.pkl', 'rb') as file:
+  encoder = pkl.load(file)
 
 st.title('Sentiment Analysis')
 
