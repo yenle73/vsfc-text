d247b369afbcd1d746d79adfe3bea58ed6cbfe37
yenle73 <128300242+yenle73@users.noreply.github.com>
2023-12-08 08:19:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ class_list = {'0': 'Negative', '1': 'Positive', '2': 'Neutral'}
 with open('ec_vsfc.pkl', 'rb') as file:
   encoder = pkl.load(file)
 
-st.title('Sentiment Analysis')
+st.title('Sentiment Analysis from Vietnamese Students\' Feedback')
 
 text = st.text_input('Enter text')
 
